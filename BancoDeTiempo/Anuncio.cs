537edbd40b258d85c898ca1c5748b8c00c4d8334
cafonsot <cafonsot@uoc.edu>
2022-10-29 18:07:07 +0200

    ClasesCS

---
+++ BancoDeTiempo/Anuncio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDeTiempo
+{
+    public class Anuncio
+    {
+
+        public int Id { get; set; }
+        public Categoria categoria { get; set; }
+        public string descripcion { get; set; }
+        public DateTime fecha { get; set; }
+
+
+    }
+}
---
+++ BancoDeTiempo/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDeTiempo
+{
+    public class Usuario
+    {
+
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public string email { get; set; }
+        public long telefono { get; set; }
+        public int horasAcumuladas { get; set; }
+        public string contraseña { get; set; }
+
+
+    }
+}
